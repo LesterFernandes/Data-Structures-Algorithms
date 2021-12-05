f3170d8f0a8c758f922c31ec6bb043e56b4ae2fa
lester <lestr.169@gmail.com>
2021-12-06 00:45:13 +0530

    git commit -m "nof palindromic substrings"

---
+++ palindromic_substrings.py
@@ -0,0 +1,23 @@
+def countSubstrings(strr: str) -> int:
+    count = 0
+    for i in range(len(strr)):
+        s, e = i, i
+        while s in range(len(strr)) and e in range(len(strr)):
+            if strr[s] != strr[e]:
+                break
+            count += 1
+            s -= 1
+            e += 1
+
+        s, e = i, i + 1
+        while s in range(len(strr)) and e in range(len(strr)):
+            if strr[s] != strr[e]:
+                break
+            count += 1
+            s -= 1
+            e += 1
+
+    return count
+
+# print(countSubstrings("aaa")) --> 6
+# print(countSubstrings("racecar")) --> 10
